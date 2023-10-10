c67a63cc8089c73e99c1618b0e8f0bc4761ba3a2
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-10 15:33:10 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+var_dump('wewewew')
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+</head>
+
+<body>
+    <h1>wewe we</h1>
+</body>
+
+</html>
